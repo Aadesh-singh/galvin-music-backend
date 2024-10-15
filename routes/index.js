eb232fac69489680b56e8eb0d188e50d69a8a2ab
Aadesh singh <aadeshsingh146@gmail.com>
2024-10-16 01:24:17 +0530

    routes updated

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const { indexRoute } = require("../controller/home");
-const user = require("./user");
+const auth = require("./auth");
 const router = express.Router();
 
 router.get("/", indexRoute);
-router.use("/user", user);
+router.use("/auth", auth);
 
 module.exports = router;
